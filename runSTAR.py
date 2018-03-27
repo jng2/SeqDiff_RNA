6fd562a82b50dd13f7fe91bb1c1f732b81d7cb44
Ian Zavitz <31973908+ianzavitz@users.noreply.github.com>
2018-03-27 15:28:26 -0500

    Create runSTAR.py

---
+++ runSTAR.py
@@ -0,0 +1,11 @@
+from subprocess import call
+#running
+call('--cd ~/star/alt_cuff-unstr')
+call('~/star/code/STAR-STAR_2.4.0k/bin/Linux_x86_64/STAR\')
+call('--runThreadN 12 --genomeDir ~/star/genome/ \')
+call('--sjdbGTFfile ~/star/Homo_sapiens.GRCh38.79.gtf --sjdbOverhang 100 \')
+call('--readFilesIn ~/star/ENCFF001RFH.fastq.gz ~/star/ENCFF001RFG.fastq.gz --readFilesCommand zcat \')
+call('--outSAMtype BAM SortedByCoordinate Unsorted \')
+call('--outSAMstrandField intronMotif')
+
+call('~/star/code/cufflinks-2.2.1.Linux_x86_64/cufflinks -p 12 Aligned.sortedByCoord.out.bam')
