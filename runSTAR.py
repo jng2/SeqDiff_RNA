1f4da2907eef2c4af24741f6ed12c1ac711b6bac
jng2 <jng2@luc.edu>
2018-03-29 00:05:58 -0500

    Update runSTAR.py

--- runSTAR.py
+++ runSTAR.py
@@ -25,3 +25,12 @@ call('--outSAMstrandField intronMotif')
 
 #p is number of threads
 call('~/star/code/cufflinks-2.2.1.Linux_x86_64/cufflinks -p 1 Aligned.sortedByCoord.out.bam')
+
+     
+## All of this under is the exact same thing as up top, but is just a little more pretty.  ALso only needs two calls, the first one makes the genome for star to use, the second is the alignment command     
+#import os
+
+#os.system("STAR --runThreadN 12 --runMode genomeGenerate --genomeDir /homes/jng2/UntitledFolder/STAR/holdme --genomeFastaFiles /homes/jng2/UntitledFolder/testgenome/dm6.fa ")
+
+#--sjdbGTFfile /homes/jng2/UntitledFolder/testgenome/dmel-all-r6.20.gtf --sjdbOverhang 100 this doesn't like any of the .gtf files for some reason
+#os.system("STAR --runMode alignReads --outSAMtype BAM SortedByCoordinate Unsorted -- readFilesCommand zcat --genomeDir /homes/jng2/UntitledFolder/STAR/holdme --readFilesIn /homes/jng2/UntitledFolder/testme.fastq.gz --runThreadN 12 --outFileNamePrefix testmeout")
