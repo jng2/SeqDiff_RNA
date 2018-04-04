4467462ac8948feb66433e77108dff2de11538a0
jng2 <jng2@luc.edu>
2018-04-04 12:09:18 -0500

    Update testGeneExpression.R

--- testGeneExpression.R
+++ testGeneExpression.R
@@ -1,21 +1,44 @@
 library("Rsamtools")
-bamfile <-BamFileList('/homes/jng2/UntitledFolder/AlignMe/C_2W_A_USR16088995L_HHFWCBBXX_L5_1_staroutAligned.out.bam',yieldSize = 2000000)
 library("GenomicFeatures")
-gtffile <- file.path('/homes/jng2/UntitledFolder/AlignMe/C_2W_A_USR16088995L_HHFWCBBXX_L5_1_staroutAligned.out.bam')
+library("BiocParallel")
+library("GenomicRanges")
+library("GenomicAlignments")
+library("DESeq2")
+library("dplyr")
+library("ggplot2")
+
+sampleTable <-read.csv("/homes/jng2/UntitledFolder/testInfoTableSmall.csv",row.names=1)
+filenames <- file.path("/homes/jng2/UntitledFolder/AlignMe",paste0(sampleTable$Run,"_staroutAligned.out.bam"))
+bamfile <-BamFileList(filenames,yieldSize = 2000000)
+
+gtffile <- file.path('/homes/jng2/UntitledFolder/testgenome/dmel-all-r6.20.gtf')
 txdb <- makeTxDbFromGFF(gtffile, format = "gtf", circ_seqs = character())
 
 ebg <- exonsBy(txdb, by="gene")
 
-register(SerialParam())
+
 se <- summarizeOverlaps(features=ebg, reads=bamfile,
                         mode="Union",
                         singleEnd=FALSE,
                         ignore.strand=TRUE,
                         fragments=TRUE )
 
+
+colData(se) <- DataFrame(sampleTable)
 colData(se)
 
+dds<-DESeqDataSet(se,design = ~ Group + Time + Group:Time)
+nrow(dds)
+dds <- dds[ rowSums(counts(dds)) > 1, ]
+nrow(dds)
 
+dds <- DESeq(dds)
+res <- results(dds)
+res
+res <- results(dds, contrast=c("Group","Control","JetLagged"))
+mcols(res, use.names = TRUE)
+summary(res)
 
-
+#count<-assay(se)
+#head(count,5)
 
