6cfaeb214a2b170c868482628717be4bbeb22080
Ian Zavitz <31973908+ianzavitz@users.noreply.github.com>
2018-04-05 16:08:06 -0500

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -19,7 +19,7 @@ for i in filelist:
   
 
   os.system("STAR --runThreadN 1 --runMode genomeGenerate --genomeDir "+genDir+" --sjdbGTFfile "+gtf+' --sjdbOverhang 100 --genomeFastaFiles '+fasta)
-  os.system("STAR --runMode alignReads --outSAMtype BAM Unsorted -- readFilesCommand zcat --genomeDir "+genDir+" --readFilesIn "+i+" --runThreadN 1 --outFileNamePrefix "+ i +".out")
+ # os.system("STAR --runMode alignReads --outSAMtype BAM Unsorted -- readFilesCommand zcat --genomeDir "+genDir+" --readFilesIn "+fastq+" --runThreadN 1 --outFileNamePrefix "+ fastq +".out")
 
   #######  BIOCONDUCTOR
   
