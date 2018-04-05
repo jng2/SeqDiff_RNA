608fac5beae70c2a2e8c1de4087778139a1c0a13
Ian Zavitz <31973908+ianzavitz@users.noreply.github.com>
2018-04-05 15:33:56 -0500

    Update and rename wrapper to wrapper.py

---
+++ wrapper.py
@@ -0,0 +1,25 @@
+import os
+
+#starting to compile the wrapper
+
+#1 input for /path/to/folder/ which contains all necessary files
+print("Enter the path to the SeqDiff directory.\nDirectory should contain\nA folder titled 'FASTQ' containing 1-10 fastq files,\nOne gtf annotation file,\nOne genome .fasta file")
+genDir = input("SeqDiff Directory path: ")
+
+filelist=os.listdir("SeqDiff/FASTQ/")
+filecount = len(filelist)
+gtf = input("Path to gtf file: ")
+fasta = ("Path to fasta file: ")
+
+
+for(i in filelist):
+  #######  STAR
+  fastq = "SeqDiff/FASTQ/" + i
+  
+
+  os.system("nohup STAR --runThreadN 1 --runMode genomeGenerate --genomeDir "+genDir+" --sjdbGTFfile "+gtf+' --sjdbOverhang 100 --genomeFastaFiles '+fasta)
+  os.system("STAR --runMode alignReads --outSAMtype BAM Unsorted -- readFilesCommand zcat --genomeDir "+genDir+" --readFilesIn "+line+" --runThreadN 1 --outFileNamePrefix fastq")
+
+  #######  BIOCONDUCTOR
+  
+  #os.system("Rscript testGeneExpression.R " + )
