a7bac8e6cbfd7688f307ba60bc9714e917ff68ea
Ian Zavitz <31973908+ianzavitz@users.noreply.github.com>
2018-04-05 15:58:44 -0500

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -5,7 +5,7 @@ import os
 #1 input for /path/to/folder/ which contains all necessary files
 print("Enter the path to the SeqDiff directory.\nDirectory should contain\nA folder titled 'FASTQ' containing 1-10 fastq files,\nOne gtf annotation file,\nOne genome .fasta file")
 #genDir = input("SeqDiff Directory path: ")
-genDir = "/homes/jng2/SeqDiff/"
+genDir = "/homes/jng2/SeqDiff"
 filelist=os.listdir(genDir + "/FASTQ/")
 filecount = len(filelist)
 gtf = str(name for name in os.listdir(genDir) if ".gtf" in name)
