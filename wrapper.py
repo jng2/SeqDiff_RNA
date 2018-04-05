0cb5597b6630384561b1a9ad2c0f1391a7cb39f5
Ian Zavitz <31973908+ianzavitz@users.noreply.github.com>
2018-04-05 16:04:09 -0500

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -18,7 +18,7 @@ for i in filelist:
   fastq = genDir+"/FASTQ/" + i
   
 
-  os.system("nohup STAR --runThreadN 1 --runMode genomeGenerate --genomeDir "+genDir+" --sjdbGTFfile "+gtf+' --sjdbOverhang 100 --genomeFastaFiles '+fasta)
+  os.system("STAR --runThreadN 1 --runMode genomeGenerate --genomeDir "+genDir+" --sjdbGTFfile "+gtf+' --sjdbOverhang 100 --genomeFastaFiles '+fasta)
   os.system("STAR --runMode alignReads --outSAMtype BAM Unsorted -- readFilesCommand zcat --genomeDir "+genDir+" --readFilesIn "+fastq+" --runThreadN 1 --outFileNamePrefix "+ fastq +".out")
 
   #######  BIOCONDUCTOR
