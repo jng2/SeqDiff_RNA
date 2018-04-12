a687fbead71a2744db4c43fb1bc854317b619ecf
jng2 <jng2@luc.edu>
2018-04-12 11:09:39 -0500

    Update testGeneExpression.R

--- testGeneExpression.R
+++ testGeneExpression.R
@@ -6,15 +6,17 @@ library("GenomicAlignments")
 library("DESeq2")
 library("dplyr")
 library("ggplot2")
+library("mygene")
 
 ptm <- proc.time()
 
 sampleTable <-read.csv("/homes/jng2/UntitledFolder/testInfoTable.csv",row.names=1)
-filenames <- file.path("/homes/jng2/UntitledFolder/AlignMe",paste0(sampleTable$Run,"_staroutAligned.out.bam"))
+filenames <- file.path("/homes/jng2/UntitledFolder/AlignMeActual",paste0(sampleTable$Run,"_staroutAligned.out.bam"))
 bamfile <-BamFileList(filenames,yieldSize = 2000000)
 
 gtffile <- file.path('/homes/jng2/UntitledFolder/testgenome/dmel-all-r6.20.gtf')
 txdb <- makeTxDbFromGFF(gtffile, format = "gtf", circ_seqs = character())
+
 
 ebg <- exonsBy(txdb, by="gene")
 
@@ -31,7 +33,7 @@ se <- summarizeOverlaps(features=ebg, reads=bamfile,
 colData(se) <- DataFrame(sampleTable)
 colData(se)
 
-dds<-DESeqDataSet(se,design = ~ Group + Time + Group:Time)
+dds<-DESeqDataSet(se,design = ~Group)
 nrow(dds)
 dds <- dds[ rowSums(counts(dds)) > 1, ]
 nrow(dds)
@@ -40,13 +42,20 @@ dds <- DESeq(dds)
 res <- results(dds)
 res
 res <- results(dds, contrast=c("Group","Control","JetLagged"))
+res.05 <- results(dds, alpha = 0.05)
+res.05sub <- subset(res.05,padj<.05)
+mcols(res.05,use.names = TRUE)
 mcols(res, use.names = TRUE)
 summary(res)
+summary(res.05)
+write.csv(res.05sub,file="2W_ControlJetlag.csv")
+#queryMany(res.05,fields="")
 
 resSig <- subset(res, padj < 0.1)
 head(resSig[ order(resSig$log2FoldChange), ])
 head(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
-
+summary(resSig[ order(resSig$log2FoldChange), ])
+summary(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
 hi<-proc.time() - ptm
 hi
 #count<-assay(se)
