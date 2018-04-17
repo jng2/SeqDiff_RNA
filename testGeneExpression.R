a49696b074dfee75b2c7974a0b54e40ac5984d34
jng2 <jng2@luc.edu>
2018-04-17 13:14:45 -0500

    Update testGeneExpression.R

--- testGeneExpression.R
+++ testGeneExpression.R
@@ -9,12 +9,12 @@ library("ggplot2")
 library("mygene")
 
 ptm <- proc.time()
-
-sampleTable <-read.csv("/homes/jng2/UntitledFolder/testInfoTable.csv",row.names=1)
-filenames <- file.path("/homes/jng2/UntitledFolder/AlignMeActual",paste0(sampleTable$Run,"_staroutAligned.out.bam"))
+args=commandArgs(trailingOnly = TRUE)
+sampleTable <-read.csv(args[1],row.names=1)
+filenames <- file.path(args[2],paste0(sampleTable$Run,"_staroutAligned.out.bam")) #naming might fuck up ><
 bamfile <-BamFileList(filenames,yieldSize = 2000000)
 
-gtffile <- file.path('/homes/jng2/UntitledFolder/testgenome/dmel-all-r6.20.gtf')
+gtffile <- file.path(args[3])
 txdb <- makeTxDbFromGFF(gtffile, format = "gtf", circ_seqs = character())
 
 
@@ -32,8 +32,8 @@ se <- summarizeOverlaps(features=ebg, reads=bamfile,
 
 colData(se) <- DataFrame(sampleTable)
 colData(se)
-
-dds<-DESeqDataSet(se,design = ~Group)
+i <- args[4]
+dds<-DESeqDataSet(se,design = ~ Group)
 nrow(dds)
 dds <- dds[ rowSums(counts(dds)) > 1, ]
 nrow(dds)
@@ -41,23 +41,29 @@ nrow(dds)
 dds <- DESeq(dds)
 res <- results(dds)
 res
-res <- results(dds, contrast=c("Group","Control","JetLagged"))
+#res <- results(dds, contrast=c("Group","Control","JetLagged"))
 res.05 <- results(dds, alpha = 0.05)
 res.05sub <- subset(res.05,padj<.05)
 mcols(res.05,use.names = TRUE)
-mcols(res, use.names = TRUE)
-summary(res)
+#mcols(res, use.names = TRUE)
+#summary(res)
 summary(res.05)
-write.csv(res.05sub,file="2W_ControlJetlag.csv")
-#queryMany(res.05,fields="")
+write.csv(res.05sub,file="testmeOut.csv")
+hi=read.csv("3W_ControlJetlagActualNewTest.csv",header=TRUE)
+hey<-getGenes(hi[,1],fields = "symbol type_of_gene")
+hey<-hey[!duplicated(hey[1]),]
 
-resSig <- subset(res, padj < 0.1)
-head(resSig[ order(resSig$log2FoldChange), ])
-head(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
-summary(resSig[ order(resSig$log2FoldChange), ])
-summary(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
-hi<-proc.time() - ptm
-hi
+
+hi$symbol<-hey$symbol
+hi$type<-hey$type_of_gene
+write.csv(hi,file="testmeOut.csv")
+#resSig <- subset(res, padj < 0.05)
+#head(resSig[ order(resSig$log2FoldChange), ])
+#head(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
+#summary(resSig[ order(resSig$log2FoldChange), ])
+#summary(resSig[ order(resSig$log2FoldChange, decreasing = TRUE), ])
+#hi<-proc.time() - ptm
+#hi
 #count<-assay(se)
 #head(count,5)
 
