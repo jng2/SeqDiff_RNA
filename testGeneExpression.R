23e971a0ac86be9dccf7c1e8066bb6575b01a146
jng2 <jng2@luc.edu>
2018-04-19 15:44:56 -0500

    Update testGeneExpression.R

--- testGeneExpression.R
+++ testGeneExpression.R
@@ -8,6 +8,11 @@ library("dplyr")
 library("ggplot2")
 library("mygene")
 
+if(args[4]='yes'){
+  source("https://bioconductor.org/biocLite.R")
+  biocLite(c("Rsamtools", "DESeq2", "GenomicFeatures","BiocParallel","GenomicRanges","GenomicAlignments","Rsamtools","mygene"))
+
+}
 ptm <- proc.time()
 args=commandArgs(trailingOnly = TRUE)
 sampleTable <-read.csv(args[1],row.names=1)
@@ -64,17 +69,4 @@ hi$symbol<-hey$symbol
 hi$type<-hey$type_of_gene
 
 write.csv(hi,file="2W_ControlJetLaggedp_1NoContstast.csv")
-
-tab = data.frame(logFC = hi$log2FoldChange, negLogPval = -log10(hi$pvalue))
-head(tab)
-par(mar = c(5, 4, 4, 4))
-plot(tab, pch = 16, cex = 0.6, xlab = expression(log[2]~fold~change), ylab = expression(-log[10]~pvalue))
-lfc = 2
-pval = 0.01
-signGenes = (abs(tab$logFC) > lfc & tab$negLogPval > -log10(pval))
-points(tab[signGenes, ], pch = 16, cex = 0.8, col = "red") 
-abline(h = -log10(pval), col = "green3", lty = 2) 
-abline(v = c(-lfc, lfc), col = "blue", lty = 2) 
-mtext(paste("pval =", pval), side = 4, at = -log10(pval), cex = 0.8, line = 0.5, las = 1) 
-mtext(c(paste("-", lfc, "fold"), paste("+", lfc, "fold")), side = 3, at = c(-lfc, lfc), cex = 0.8, line = 0.5)
 
